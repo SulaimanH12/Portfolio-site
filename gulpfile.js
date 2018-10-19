70ce04e7368abc9ba92b63f64a760bf899fc07ee
SulaimanH12 <sulaiman96@hotmail.co.uk>
2018-10-19 22:14:20 +0100

    updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,109 +1,107 @@
-// Plugins
-var gulp = require("gulp"),
-    sass = require("gulp-sass"),
-    concat = require("gulp-concat"),
-    watch = require("gulp-watch"),
-    plumber = require("gulp-plumber"),
-    minify_css = require("gulp-clean-css"),
-    uglify = require("gulp-uglify"),
-    sourcemaps = require("gulp-sourcemaps"),
-    imagemin = require("gulp-imagemin"),
-    notify = require("gulp-notify"),
-    prefix = require("gulp-autoprefixer"),
-    jshint = require("gulp-jshint"),
-    pngquant = require("imagemin-pngquant"),
-    browserSync = require("browser-sync");
+const    gulp = require("gulp");
+const    sass = require("gulp-sass");
+const    htmlmin = require("gulp-htmlmin");
+const    cleancss = require("gulp-clean-css");
+const    postcss = require('gulp-postcss');
+const    uncss = require("gulp-uncss");
+const    responsive = require("gulp-responsive");
+const    notify = require("gulp-notify");
+const    stripDebug = require("gulp-strip-debug");
+const    concat = require("gulp-concat");
+const    watch = require("gulp-watch");
+const    plumber = require("gulp-plumber");
+const    uglify = require("gulp-uglify");
+const    sourcemaps = require("gulp-sourcemaps");
+const    imagemin = require("gulp-imagemin");
+const    autoprefixer = require("autoprefixer");
+const    browser_sync = require("browser-sync");
 
-// ----------------------------------------------------
+// ------
 
-// Settings
-var assets = {
+const assets = {
     js: "assets/js",
     css: "assets/css",
     img: "assets/img",
-    html: "assets/*.html",
+    html: "assets/**/*html",
     min_css: 'style.min.css',
     min_js: 'app.min.js'
 };
 
-var src = {
+const src = {
     sass: "src/sass/**/*.scss",
     js: "src/js/**/*.js",
     img: "src/img/*"
 };
 
-// ----------------------------------------------------
+// ------
 
-// Error Handler 
-var onError = function(err) {
+// Error Handler
+const onError = (err) => {
     console.log(err);
     this.emit('end');
 };
 
-// ----------------------------------------------------
-
-// Sass to Css
-gulp.task('sass', function() {
+// Sass to CSS
+gulp.task("sass", () => {
     return gulp.src(src.sass)
-        .pipe(plumber({
-            errorHandler: onError
-        }))
+        .pipe(plumber())
         .pipe(sass())
-        .pipe(prefix('last 2 versions'))
-        .pipe(concat('style.min.css'))
+        .pipe(cleancss())
+        .pipe(concat("style.min.css"))
         .pipe(gulp.dest(assets.css))
-        .pipe(minify_css())
         .pipe(sourcemaps.init())
+        .pipe(postcss([autoprefixer()], [uncss({
+            html:['index.html']
+        })]))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(assets.css))
-        .pipe(browserSync.reload({stream: true}));
+        .pipe(browser_sync.reload({stream: true}));
 });
 
-// ----------------------------------------------------
+// ------
 
 // Compile JS
-gulp.task('js', function() {
+gulp.task("js", () => {
     return gulp.src(src.js)
         .pipe(plumber({
             errorHandler: onError
         }))
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'))
         .pipe(uglify())
-        .pipe(concat('app.min.js'))
+        .pipe(stripDebug())
+        .pipe(concat("app.min.js"))
         .pipe(sourcemaps.init())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(assets.js))
-        .pipe(browserSync.reload({ stream: true }));
+        .pipe(browser_sync.reload({stream: true}));
 });
 
-// ----------------------------------------------------
+// ------
 
 // Images
- gulp.task('img', function() {
+gulp.task("img", () => {
     return gulp.src(src.img)
-    .pipe(imagemin({
-        progressive: true,
-        svgoPlugins: [{removeViewBox: false}],
-        use: [pngquant()]
-    }))
-    .pipe(gulp.dest(assets.img));
- });
+        .pipe(imagemin({
+            interlaced: true,
+            progressive: true,
+            optimizationLevel: 5,
+            svgoPlugins: [{removeViewBox: false}]
+        }))
+        .pipe(gulp.dest(assets.img));
+})
 
-// ----------------------------------------------------
+// ------
 
 // Watch
-gulp.task('watch', function() {
-    browserSync.init({
+gulp.task('watch', () => {
+    browser_sync.init({
         server: './assets'
-    });
+    })
     gulp.watch(src.js, ['js']);
     gulp.watch(src.sass, ['sass']);
-    gulp.watch(src.img, ['img']);
-    gulp.watch('index.html').on('change', browserSync.reload);
+    gulp.watch(src.img['img']);
+    gulp.watch(assets.html).on('change', browser_sync.reload);
 });
 
-// ----------------------------------------------------
 
 // Default
-gulp.task('default', ['watch', 'sass', 'js', 'img']);
+gulp.task("default", ["watch", "js", "sass", "img"]);
