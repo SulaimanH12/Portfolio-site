13a7c3548b9306a4888c1a127cef0804587f9044
SulaimanH12 <sulaiman96@hotmail.co.uk>
2018-11-01 16:50:00 +0000

    adding jquery and more menu animation

--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,24 @@ menuToggle.onclick = function() {
 closeMenu.onclick = function() {
     mobileMenu.style.transform = "translateX(100%)";
 }
+
+// Scroll to  target given anchor with an id
+$("a[href^=\\#]").on('click', function (e) {
+
+    // prevents native anchor behavior
+    e.preventDefault();
+
+    // store destination
+    var dest = $(this).attr('href');
+
+    // close mobile menu
+    $("nav.mobile-menu")
+        .removeClass("open")
+        .css("transform", "translateX(100%)");
+
+
+    // animate towards the dest via 2ms
+    $('html, body').animate({
+        scrollTop: $(dest).offset().top
+    }, 2000);
+});
